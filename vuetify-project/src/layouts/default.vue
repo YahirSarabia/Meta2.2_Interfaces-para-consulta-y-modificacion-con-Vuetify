058ec166dfc7d00088161bec3564411e39da0dfa
YahirSarabia <158230448+YahirSarabia@users.noreply.github.com>
2024-09-21 18:36:35 -0700

    Initial commit

--- vuetify-project/src/layouts/default.vue
+++ vuetify-project/src/layouts/default.vue
@@ -28,35 +28,8 @@
 import { ref } from 'vue';
 const drawer = ref(false);
 
-
 function onDrawer(){
   drawer.value = !drawer.value
 }
 
 </script>
-
-
-<!--
-<template>
-  <v-app>
-    <v-layout class="rounded rounded-md">
-    <v-app-bar title="Application bar"></v-app-bar>
-
-    <v-navigation-drawer>
-      <v-list>
-        <v-list-item title="Navigation drawer"></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-main>
-      <router-view />
-    </v-main>
-    <AppFooter />
-  </v-layout>
-  </v-app>
-</template>
-
-<script setup>
-  //
-</script>
--->
